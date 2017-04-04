d599677e131e07dcb126ab1814e45ce8c915e7fd
Daniel Janzon <janzon@gmail.com>
2017-04-04 19:49:47 +0200

    Refactor code that replaces macro arguments with the call arguments.

--- qcodecompiler.py
+++ qcodecompiler.py
@@ -28,25 +28,25 @@ def unroll_sequence(sequence, call_symbols):
 
     return new_seq
 
+def get_macro_to_macrocall_arguments_map(macro_args, macrocall_args):
+    call_symbols = {}
+    for (i, arg) in enumerate(macro_args):
+        call_symbols[arg] = macrocall_args[i]
+    return call_symbols
 
 def compile_sequence(qcode, sequence):
     gate_array = []
     for item in sequence.elements:
 
         if isinstance(item, Operation):
-
             gate = str_to_gate[item.operator]
             indices = [qcode.registers[argument].qbits[0] for
                        argument in item.arguments]
             gate_array.append(gate(*indices))
 
         elif isinstance(item, MacroCall):
-
             macro = qcode.macros[item.name]
-            call_symbols = {}
-            for (i, arg) in enumerate(macro.arguments):
-                call_symbols[arg] = item.arguments[i]
-
+            call_symbols = get_macro_to_macrocall_arguments_map(macro.arguments, item.arguments)
             unrolled = unroll_sequence(macro.sequence, call_symbols)
             gate_array.append(compile_sequence(qcode, unrolled))
 
