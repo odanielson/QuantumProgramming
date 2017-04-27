c5540595178c182d37758be7291aa12248686b92
Olov Danielson <olov.danielson@gmail.com>
2017-04-27 19:38:11 +0200

    Add gatearray docstring and add Identity operator

--- gatearray.py
+++ gatearray.py
@@ -1,10 +1,21 @@
+"""A gatearray is a list of gates representing a quantum program.
+
+Important notes:
+
+  - qubit index start at 0
+
+"""
+
 from collections import namedtuple
+
 
 START = namedtuple('START', 'n')
 X = namedtuple('X', 'i')
 CNOT = namedtuple('CNOT', 'ctrl target')
 H = namedtuple('H', 'i')
 M = namedtuple('M', 'i')
+I = namedtuple('I', 'i')
+
 
 str_to_gate = {
     'X': X,
