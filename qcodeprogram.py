74fb3afb0c879ed00887fe88062743fed2091772
Olov Danielson <olov.danielson@gmail.com>
2017-04-27 19:39:37 +0200

    Run qcodeprogram in ideal simulator

--- qcodeprogram.py
+++ qcodeprogram.py
@@ -1,3 +1,4 @@
+from simulator import run_gate_array
 from qcodecompiler import qcompile
 from qcode import QCode, Sequence, Register, Macro, MacroCall, Operation
 
@@ -7,6 +8,7 @@ if __name__ == "__main__":
     code = QCode()
     code.add_register('q0', Register([0,1]))
     code.add_register('q1', Register([2]))
+    code.add_register('q2', Register([3, 11]))
 
     hello_sequence = Sequence()
     hello_sequence.add(Operation('X', ['a']))
@@ -17,6 +19,7 @@ if __name__ == "__main__":
     program_sequence = Sequence()
     program_sequence.add(MacroCall('hello', ['q0']))
     program_sequence.add(MacroCall('hello', ['q1']))
+    program_sequence.add(Operation('X', ['q2']))
     code.add_program(program_sequence)
 
 
@@ -24,6 +27,8 @@ if __name__ == "__main__":
 
     gate_array = qcompile(code)
     print gate_array
+
+    run_gate_array(gate_array)
 
     # Should result in this gate array
     # gate_array = [
