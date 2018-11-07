3830dcdb81043b23b95535d753483d910b2ced5f
Daniel Janzon <janzon@gmail.com>
2018-11-07 22:45:25 +0100

    Trim probabilities since small imaginary parts can make the probability negative which is not accepted by random.choice in numpy.

--- measure.py
+++ measure.py
@@ -9,12 +9,19 @@ def get_bit(i, k, n):
     return (k >> (n-1-i)) & 0x1
 
 
+def trim_probabilities(p):
+    """Small and negative(!) probabilities will be set to exactly zero."""
+    e = np.finfo(dtype=np.complex256).eps  # Smallest number s.t. 1.0+e != 1.0
+    return [x if x > 2*e else 0 for x in p]
+
+
 def measure(qubits, i):
     """Measure qubit `i` in `qubits` and return 0 or 1. Index `i` from 0."""
     assert i < qubits.n, (
         "qubit %d can not be measured in %d-qubit state" % (i, qubits.n))
     distribution = qubits.distribution()
     p = np.squeeze(np.asarray(distribution, dtype=np.float64))
+    p = trim_probabilities(p)
     index = np.random.choice(np.arange(2**qubits.n), p=p)
     result = get_bit(i, index, qubits.n)
     projection = ToZero if result == 0 else ToOne
