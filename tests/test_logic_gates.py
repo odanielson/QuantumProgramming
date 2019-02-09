a19105d93a0b48e6753f09dd035fc50a4f5abac5
Olov Danielson <olov.danielson@gmail.com>
2019-02-09 19:04:37 +0100

    Remove rev_CNOT from library

--- tests/test_logic_gates.py
+++ tests/test_logic_gates.py
@@ -36,32 +36,3 @@ def test_or():
 
         result = run(program, run_gate_array, return_distribution=True)
         assert isclose(result, facit).all(), "%s failed" % program
-
-
-def test_rev_CNOT():
-
-    table = {
-        (0,0): [1, 0, 0, 0],  # -> 0 0
-        (0,1): [0, 0, 0, 1],  # -> 1 1
-        (1,0): [0, 0, 1, 0],  # -> 1 0
-        (1,1): [0, 1, 0, 0]   # -> 0 1
-    }
-
-    for setup, facit in table.iteritems():
-
-        setup = tuple(["X q%d" % i if v==1 else ""
-                       for i, v in enumerate(setup)])
-        program = dedent("""\
-            register q0[0]
-            register q1[1]
-
-            include qclib/reversed_gates.qc
-
-            %s
-            %s
-
-            rev_CNOT q1 q0
-        """ % setup)
-
-        result = run(program, run_gate_array, return_distribution=True)
-        assert isclose(result, facit).all(), "%s failed" % program
