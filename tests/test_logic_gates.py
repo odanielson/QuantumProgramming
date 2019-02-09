e2c4ff6f1196be67e7f9a5e2f076620aae50193c
Olov Danielson <olov.danielson@gmail.com>
2019-02-09 14:23:55 +0100

    Add or gate test case

---
+++ tests/test_logic_gates.py
@@ -0,0 +1,38 @@
+
+from textwrap import dedent
+
+from numpy import isclose
+
+from drivers.vector.simulator import run_gate_array
+from qrun import run
+
+
+def test_or():
+
+    table = {
+        (0,0,0): [1,0,0,0,0,0,0,0],  # -> 0 0 0
+        (0,1,0): [0,0,0,1,0,0,0,0],  # -> 0 1 1
+        (1,0,0): [0,0,0,0,0,1,0,0],  # -> 1 0 1
+        (1,1,0): [0,0,0,0,0,0,0,1],  # -> 1 1 1
+    }
+
+    for setup, facit in table.iteritems():
+
+        setup = tuple(["X q%d" % i if v==1 else ""
+                       for i, v in enumerate(setup)])
+        program = dedent("""\
+            register q0[0]
+            register q1[1]
+            register q2[2]
+
+            include qclib/classical_logic.qc
+
+            %s
+            %s
+            %s
+
+            or q0 q1 q2
+        """ % setup)
+
+        result = run(program, run_gate_array, return_distribution=True)
+        assert isclose(result, facit).all(), "%s failed" % program
